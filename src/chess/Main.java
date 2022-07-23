4b12431068eee547afe45d2c855e7992d1fcf061
gtopinio <mctopinio@up.edu.ph>
2022-07-23 23:28:47 +0800

    Created Main.java & images folder
    - Replaced App.java with Main.java
    - Added small documentation of chess app in Main.java
    - Placed Main.java in "chess" package folder
    - Main Class inherited the Application Class
    - Added pictures of chess board and pieces in images folder

---
+++ src/chess/Main.java
@@ -0,0 +1,22 @@
+package chess;
+/***********************************************************
+* This is a Java program that simulates a Chess application
+*
+* @author Mark Genesis C. Topinio
+* @created_date 2022-07-23 21:30
+*
+***********************************************************/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static void main(String[] args) {
+		launch(args);
+	}
+
+    public void start(Stage stage){
+        // Starting the Chess App
+	}
+}
